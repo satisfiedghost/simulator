2264e36b05023418dd1a99e861cb486b7e935068
Brennan Ruthardt <brennanruthardt@gmail.com>
2021-12-28 18:56:16 -0800

    Add Unit Tests
    Added a basic unit test.

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,13 @@ int main() {
   Simulation::Vector<float> v1(5, 0, 0);
   Simulation::Vector<float> v2(-5, 0, 0);
   Simulation::Vector<float> p1(-200, 0, 0);
-  Simulation::Vector<float> p2(200, 19, 0);
+  Simulation::Vector<float> p2(200, 11, 0);
+  Simulation::Vector<float> v3(0, 0, 0);
+  Simulation::Vector<float> p3(0, 25, 0);
 
   sim.add_particle(v1, p1);
   sim.add_particle(v2, p2);
+  sim.add_particle(v3, p3);
   sim.run(10000000);
 
   return 0;
---
+++ tst/vector.cc
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "vector.h"
+#include <vector>
+
+std::vector<float> ones{1, -1};
+
+class CombinationsTest :
+    public ::testing::TestWithParam<std::tuple<float, float, float>> {};
+
+// Magnitude Calculation
+TEST_P(CombinationsTest, Magnitude) {
+  Simulation::Vector<float> test1(std::get<0>(GetParam()),
+                                  std::get<1>(GetParam()),
+                                  std::get<2>(GetParam()));
+  ASSERT_FLOAT_EQ(test1.magnitude, std::sqrt(3));
+}
+
+INSTANTIATE_TEST_CASE_P(AllCombinations,
+                        CombinationsTest,
+                        ::testing::Combine(::testing::ValuesIn(ones),
+                                           ::testing::ValuesIn(ones),
+                                           ::testing::ValuesIn(ones)));
+
